c8a3760540e16d35c63882dd68de91ced8cf4641
Afraaz <ahmedalif102@gmail.com>
2023-11-05 12:38:42 +0600

    handleAddToCart function implemented

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,17 +1,19 @@
-import './Product.css'
+import "./Product.css";
 
-const Product = ({product}) => {
- const {img, ratings, name, seller, price, quantity} = product
+const Product = ({ product, handleAddToCart }) => {
+  const { img, ratings, name, seller, price, quantity } = product;
   return (
     <div className="product">
       <img src={img} alt="" />
-      <div className='product-info'>
+      <div className="product-info">
         <h5 className="product-name">{name}</h5>
         <p>Price: ${price}</p>
         <p>Manufacturer : {seller}</p>
         <p>Rating : {ratings} star</p>
       </div>
-      <button className='btn-cart'>Add to Cart</button>
+      <button onClick={() => handleAddToCart(product)} className="btn-cart">
+        Add to Cart
+      </button>
     </div>
   );
 };
